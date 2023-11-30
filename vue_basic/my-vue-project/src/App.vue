88a971a4c67c948879c186b70deea71fa65dfcb3
xiaoxiong <1651770254@qq.com>
2023-11-30 20:02:36 +0800

    脚手架与组件开发

---
+++ vue_basic/my-vue-project/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <h1>{{ childCount }}</h1>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld>111</HelloWorld> -->
+    <HelloWorld
+      msg="Welcome to Your Vue.js App"
+      :count="parentCount"
+      @child_methods="parent_methods"
+      >阿斯顿
+      <!-- <template #footer="dataObj">footer插槽:{{ dataObj.num }}</template> -->
+      <template #footer="{ num }">footer插槽:{{ num }}</template>
+    </HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      parentCount: 1100,
+      childCount: 0,
+    };
+  },
+  methods: {
+    parent_methods(childCount) {
+      this.childCount = childCount;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue_basic/my-vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <slot>基础的默认内容</slot>
+    <h1>{{ msg }}</h1>
+    <h1>props的count{{ count }}</h1>
+    <button @click="child_1">按钮</button>
+    <slot name="footer" :num="num">footer的默认内容</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  // 1.父传子
+  props: {
+    msg: String,
+    count: {
+      type: [String, Number],
+      default: 100,
+      // required: true
+    },
+  },
+  data() {
+    return { num: 0 };
+  },
+  // 子传父
+  methods: {
+    child_1() {
+      this.num++, this.$emit("child_methods", this.num);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
