446297ea9e7fdda99b2977614b778aac8f60f313
xiaoxiong <1651770254@qq.com>
2024-03-10 21:17:51 +0800

    setup返回值

---
+++ vue3/hello_vue3/src/components/Person.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">年龄+1</button>
+    <button @click="showTel">点我查看联系方式</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Person',
+  setup () {
+    // 数据，原来写在data中（注意：此时的name、age、tel数据都不是响应式数据）
+    let name = '张三'
+    let age = 18
+    let tel = '13888888888'
+
+    // 方法，原来写在methods中
+    function changeName () {
+      name = 'zhang-san' //注意：此时这么修改name页面是不变化的
+      console.log(name)
+    }
+    function changeAge () {
+      age += 1 //注意：此时这么修改age页面是不变化的
+      console.log(age)
+    }
+    function showTel () {
+      alert(tel)
+    }
+
+    // 返回一个对象，对象中的内容，模板中可以直接使用
+    return { name, age, tel, changeName, changeAge, showTel }
+  }
+}
+</script>
+<style>
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+button {
+  margin: 0px 5px;
+}
+</style>
--- vue3/hello_vue3/src/main.ts
+++ vue3/hello_vue3/src/main.ts
@@ -1,6 +1,6 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+// 引入createApp用于创建应用（买个盆）
+import { createApp } from "vue";
+// 引入App根组件（买个根）
+import App from "./App.vue";
+// 将根放在盆里，并将盆放在id为app的div位置上
+createApp(App).mount("#app");
