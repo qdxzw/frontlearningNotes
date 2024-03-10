030f0f37c98d22bb32cb857ff0cfc9b14646470e
xiaoxiong <1651770254@qq.com>
2024-03-10 23:07:22 +0800

    setup语法糖

--- vue3/hello_vue3/src/components/Person.vue
+++ vue3/hello_vue3/src/components/Person.vue
@@ -7,33 +7,23 @@
     <button @click="showTel">点我查看联系方式</button>
   </div>
 </template>
+<script lang="ts" setup name="Person123">
+// 数据，原来写在data中（注意：此时的name、age、tel数据都不是响应式数据）
+let name = '张三'
+let age = 18
+let tel = '13888888888'
 
-<script lang="ts">
-export default {
-  name: 'Person',
-  setup () {
-    // 数据，原来写在data中（注意：此时的name、age、tel数据都不是响应式数据）
-    let name = '张三'
-    let age = 18
-    let tel = '13888888888'
-
-    // 方法，原来写在methods中
-    function changeName () {
-      name = 'zhang-san' //注意：此时这么修改name页面是不变化的
-      console.log(name)
-    }
-    function changeAge () {
-      age += 1 //注意：此时这么修改age页面是不变化的
-      console.log(age)
-    }
-    function showTel () {
-      alert(tel)
-    }
-
-    // 返回一个对象，对象中的内容，模板中可以直接使用
-    // return { name, age, tel, changeName, changeAge, showTel }
-    return () => '你好啊！'
-  }
+// 方法，原来写在methods中
+function changeName () {
+  name = 'zhang-san' //注意：此时这么修改name页面是不变化的
+  console.log(name)
+}
+function changeAge () {
+  age += 1 //注意：此时这么修改age页面是不变化的
+  console.log(age)
+}
+function showTel () {
+  alert(tel)
 }
 </script>
 <style>
