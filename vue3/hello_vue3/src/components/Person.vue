4e9fef7eb85a3a5fa44f38593250fb80617a70e8
xiaoxiong <1651770254@qq.com>
2024-04-24 18:27:08 +0800

    watch情况二

--- vue3/hello_vue3/src/components/Person.vue
+++ vue3/hello_vue3/src/components/Person.vue
@@ -1,26 +1,44 @@
 <template>
   <div class="person">
-    <h1>情况一：监视【ref】定义的【基本类型】数据</h1>
-    <h2>当前求和为：{{ sum }}</h2>
-    <button @click="changeSum">点我sum+1</button>
+    <h1>情况二：监视【ref】定义的【对象类型】数据</h1>
+    <h2>姓名：{{ person.name }}</h2>
+    <h2>年龄：{{ person.age }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="changePerson">修改整个人</button>
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
 import { ref, watch } from 'vue'
 // 数据
-let sum = ref(0)
-// 方法
-function changeSum () {
-  sum.value += 1
-}
-// 监视，情况一：监视【ref】定义的【基本类型】数据
-const stopWatch = watch(sum, (newValue, oldValue) => {
-  console.log('sum变化了', newValue, oldValue)
-  if (newValue >= 10) {
-    stopWatch()
-  }
+let person = ref({
+  name: '张三',
+  age: 18
 })
+// 方法
+function changeName () {
+  person.value.name += '~'
+}
+function changeAge () {
+  person.value.age += 1
+}
+function changePerson () {
+  person.value = { name: '李四', age: 90 }
+}
+/* 
+    监视，情况一：监视【ref】定义的【对象类型】数据，监视的是对象的地址值，若想监视对象内部属性的变化，需要手动开启深度监视
+    watch的第一个参数是：被监视的数据
+    watch的第二个参数是：监视的回调
+    watch的第三个参数是：配置对象（deep、immediate等等.....） 
+  */
+watch(
+  person,
+  (newValue, oldValue) => {
+    console.log('person变化了', newValue, oldValue)
+  },
+  { deep: true }
+)
 </script>
 
 <style>
