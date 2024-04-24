1bdcd715b1b16e78088f13ed211742789dafb474
xiaoxiong <1651770254@qq.com>
2024-04-24 16:27:05 +0800

    ref 创建（对象类型的响应式数据）

--- vue3/hello_vue3/src/components/Person.vue
+++ vue3/hello_vue3/src/components/Person.vue
@@ -5,41 +5,27 @@
     <ul>
       <li v-for="g in games" :key="g.id">{{ g.name }}</li>
     </ul>
-    <h2>测试：{{ obj.a.b.c.d }}</h2>
     <button @click="changeCarPrice">修改汽车价格</button>
     <button @click="changeFirstGame">修改第一游戏</button>
-    <button @click="test">测试</button>
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { reactive } from 'vue'
+import { ref } from 'vue'
 
 // 数据
-let car = reactive({ brand: '迈巴赫', price: 200 })
-let games = reactive([
+let car = ref({ brand: '迈巴赫', price: 200 })
+let games = ref([
   { id: 'ahsgdyfa01', name: '英雄联盟' },
   { id: 'ahsgdyfa02', name: '王者荣耀' },
   { id: 'ahsgdyfa03', name: '原神' }
 ])
-let obj = reactive({
-  a: {
-    b: {
-      c: {
-        d: 666
-      }
-    }
-  }
-})
 
 function changeCarPrice () {
-  car.price += 10
+  car.value.price += 10
 }
 function changeFirstGame () {
-  games[0].name = '流星蝴蝶剑'
-}
-function test () {
-  obj.a.b.c.d = 999
+  games.value[0].name = '流星蝴蝶剑'
 }
 </script>
 
