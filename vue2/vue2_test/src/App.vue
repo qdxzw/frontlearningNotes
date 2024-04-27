f9909643bf2c70169f102406aa4d1d02d47f762e
xiaoxiong <1651770254@qq.com>
2024-04-27 16:22:16 +0800

    vue2生命周期

---
+++ vue2/vue2_test/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app"><Person v-if="isshow" /></div>
+</template>
+
+<script>
+import Person from './components/Person.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Person
+  },
+  data () {
+    return {
+      isshow: true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
