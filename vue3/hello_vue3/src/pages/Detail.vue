2a83f196fb7dfdf539c7aa5a447d7965e61521b9
xiaoxiong <1651770254@qq.com>
2024-04-28 11:56:02 +0800

    路由的props配置

--- vue3/hello_vue3/src/pages/Detail.vue
+++ vue3/hello_vue3/src/pages/Detail.vue
@@ -1,16 +1,13 @@
 <template>
   <ul>
-    <li>编号：{{ params.id }}</li>
-    <li>标题：{{ params.title }}</li>
-    <li>内容：{{ params.content }}</li>
+    <li>编号：{{ id }}</li>
+    <li>标题：{{ title }}</li>
+    <li>内容：{{ content }}</li>
   </ul>
 </template>
 
 <script lang="ts" setup name="Detail">
-import { useRoute } from 'vue-router'
-import { toRefs } from 'vue'
-let route = useRoute()
-let { params } = toRefs(route)
+defineProps(['id', 'title', 'content'])
 </script>
 
 <style scoped>
--- vue3/hello_vue3/src/router/index.ts
+++ vue3/hello_vue3/src/router/index.ts
@@ -21,6 +21,16 @@ const router = createRouter({
           name: "xiang",
           path: "detail/:id/:title/:content",
           component: Detail,
+          // 第一种(传递值给路由组件)：props的对象写法，作用：把对象中的每一组key-value作为props传给Detail组件
+          // props:{a:1,b:2,c:3},
+
+          // 第二种(只适用于props)：props的布尔值写法，作用：把收到了每一组params参数，作为props传给Detail组件
+          props: true,
+
+          // 第三种(适用于props、query)：props的函数写法，作用：把返回的对象中每一组key-value作为props传给Detail组件
+          // props(route) {
+          //   return route.query;
+          // },
         },
       ],
     },
