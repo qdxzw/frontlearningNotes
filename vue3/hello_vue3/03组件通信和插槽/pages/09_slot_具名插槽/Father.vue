37bb2c07dfa022f40b23718930fdeb3fa19aa239
xiaoxiong <1651770254@qq.com>
2024-05-01 23:39:31 +0800

    插槽的学习

--- vue3/hello_vue3/03组件通信和插槽/pages/09_slot_具名插槽/Father.vue
+++ vue3/hello_vue3/03组件通信和插槽/pages/09_slot_具名插槽/Father.vue
@@ -15,7 +15,7 @@
 
       <Category>
         <template v-slot:s2>
-          <img :src="imgUrl" alt="">
+          <img :src="imgUrl" alt="" />
         </template>
         <template v-slot:s1>
           <h2>今日美食城市</h2>
@@ -35,37 +35,37 @@
 </template>
 
 <script setup lang="ts" name="Father">
-  import Category from './Category.vue'
-  import { ref,reactive } from "vue";
+import Category from './Category.vue'
+import { ref, reactive } from 'vue'
 
-  let games = reactive([
-    {id:'asgytdfats01',name:'英雄联盟'},
-    {id:'asgytdfats02',name:'王者农药'},
-    {id:'asgytdfats03',name:'红色警戒'},
-    {id:'asgytdfats04',name:'斗罗大陆'}
-  ])
-  let imgUrl = ref('https://z1.ax1x.com/2023/11/19/piNxLo4.jpg')
-  let videoUrl = ref('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4')
-
+let games = reactive([
+  { id: 'asgytdfats01', name: '英雄联盟' },
+  { id: 'asgytdfats02', name: '王者农药' },
+  { id: 'asgytdfats03', name: '红色警戒' },
+  { id: 'asgytdfats04', name: '斗罗大陆' }
+])
+let imgUrl = ref('https://z1.ax1x.com/2023/11/19/piNxLo4.jpg')
+let videoUrl = ref('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4')
 </script>
 
 <style scoped>
-  .father {
-    background-color: rgb(165, 164, 164);
-    padding: 20px;
-    border-radius: 10px;
-  }
-  .content {
-    display: flex;
-    justify-content: space-evenly;
-  }
-  img,video {
-    width: 100%;
-  }
-  h2 {
-    background-color: orange;
-    text-align: center;
-    font-size: 20px;
-    font-weight: 800;
-  }
+.father {
+  background-color: rgb(165, 164, 164);
+  padding: 20px;
+  border-radius: 10px;
+}
+.content {
+  display: flex;
+  justify-content: space-evenly;
+}
+img,
+video {
+  width: 100%;
+}
+h2 {
+  background-color: orange;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 800;
+}
 </style>
