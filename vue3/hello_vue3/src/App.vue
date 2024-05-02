eeca5aee540ee09fa93c32c20f6988310dbe9a87
xiaoxiong <1651770254@qq.com>
2024-05-02 14:27:50 +0800

    Teleport

--- vue3/hello_vue3/src/App.vue
+++ vue3/hello_vue3/src/App.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="app">
-    <h2>{{ msg }}</h2>
-    <input type="text" v-model="msg" />
+  <div class="outer">
+    <h2>我是父组件</h2>
+    <img src="http://www.atguigu.com/images/index_new/logo.png" alt="" />
+    <br />
+    <modal></modal>
   </div>
 </template>
 
 <script setup lang="ts" name="App">
-import { ref } from 'vue'
-import useMsgRefs from './useMsgRefs'
-// let msg = ref('你好')
-let { msg } = useMsgRefs('你好', 1000)
+import modal from './modal.vue'
 </script>
 
 <style scoped>
-.app {
+.outer {
+  width: 400px;
+  height: 300px;
   border-radius: 10px;
   box-shadow: 0 0 10px;
   background-color: #ddd;
+  filter: saturate(200%);
+}
+img {
+  width: 180px;
 }
 button {
   margin: 0 5px;
---
+++ vue3/hello_vue3/src/modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="isShow = true">显示弹窗</button>
+  <teleport to="body">
+    <div class="modal" v-show="isShow">
+      <h2>我是一个弹窗</h2>
+      <p>我是弹窗中的一些内容</p>
+      <button @click="isShow = false">关闭弹窗</button>
+    </div>
+  </teleport>
+</template>
+
+<script setup lang="ts" name="modal">
+import { ref } from 'vue'
+let isShow = ref(false)
+</script>
+
+<style scoped>
+.modal {
+  width: 300px;
+  text-align: center;
+  background-color: skyblue;
+  border-radius: 10px;
+  box-shadow: 0 0 10px;
+  position: fixed;
+  top: 20px;
+  left: 50%;
+  margin-left: -150px;
+}
+</style>
